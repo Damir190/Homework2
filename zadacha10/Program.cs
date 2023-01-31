ed0659f4a7da697d049b047120eae9378a2ae545
Damir <ag_cmg@mail.ru>
2023-01-31 20:49:16 +0300

    zad10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число : ");
+int num = int.Parse(Console.ReadLine()!);
+
+int a1 = num / 1000;
+int a2 = num % 100;
+int a3 = num % 10;
+int result = (a2 - a1 - a3) / 10;
+
+if(num < 100 || num > 1000 ) 
+{
+    Console.WriteLine($"Число {num} не является трехзначным! ");
+}
+else Console.WriteLine($"{num} -> {result}");
+
