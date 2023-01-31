ce311249a9b55e962f1a88f8210151202fe5c35c
Damir <ag_cmg@mail.ru>
2023-01-31 20:55:44 +0300

    zad13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// (Цифры считать справа налево).
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число : ");
+int num = int.Parse(Console.ReadLine()!);
+int result = num / 100;
+
+if(num < 100 || num > 1000 ) 
+{
+    Console.WriteLine($"Число {num} не является трехзначным! ");
+}
+else Console.WriteLine($"{num} -> {result}");
+
